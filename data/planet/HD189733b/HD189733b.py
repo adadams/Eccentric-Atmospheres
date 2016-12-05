07a304d5236f4604358f74ccfd7b4e30adc58cc0
Arthur Adams <arthur.daniel.adams@gmail.com>
2016-12-05 14:53:35 -0500

    Multiple orbit plots; added HD 189733b data.

---
+++ data/planet/HD189733b/HD189733b.py
@@ -0,0 +1,39 @@
+###################################################################
+#   HD 189733b DATA FILE
+###################################################################
+
+import numpy as N
+import astropy.constants as C
+import astropy.units as U
+
+#The published properties of the planetary system.
+system_properties = {
+    'name': 'HD189733b',
+    #P
+    'orbital period': 2.218575143 * U.d,
+    #a
+    'semimajor axis': 0.0312 * U.AU,
+    #e
+    'eccentricity': 0.,
+    #w
+    'argument of periastron': 90 * U.deg,
+    #mp
+    'planet mass': 1.162 * C.M_jup,
+    #rp
+    'planet radius': 1.216 * C.R_jup,
+    #M
+    'stellar mass': 0.846 * C.M_sun,
+    #R
+    'stellar radius': 0.805 * C.R_sun,
+    #Teff
+    'stellar temperature': 4875 * U.K
+         }
+
+#The directory for the data.
+data = {'3p6': N.genfromtxt('data/planet/HD189733b/3p6.csv', comments='#', delimiter=',', names=True),
+        '4p5': N.genfromtxt('data/planet/HD189733b/4p5.csv', comments='#', delimiter=',', names=True)}
+
+for band in data:
+    data[band]['t'] = data[band]['t'] % 1
+    data[band]['t'] = N.where(data[band]['t'] > 0.5, data[band]['t'] - 1, data[band]['t'])
+    data[band]['t'] = data[band]['t'] * system_properties['orbital period']
--- visualization/lightcurve_plot.py
+++ visualization/lightcurve_plot.py
@@ -25,26 +25,32 @@ class LightCurvePlot:
 
     def draw(self, axis=plt.gca(), save=False):
         for band in self.data:
-            low_cut = (self.planet_object.times - (self.planet_object.P*(self.planet_object.num_orbits))) / U.d >= N.min(self.data[band]['t'])
-            high_cut = (self.planet_object.times - (self.planet_object.P*(self.planet_object.num_orbits))) / U.d <= N.max(self.data[band]['t'])
-            plot_cut = low_cut & high_cut
-
-            plotted_times = self.planet_object.times[plot_cut] - (self.planet_object.P*(self.planet_object.num_orbits))
+            plot_cuts = []
+            plotted_times = []
+            for orbits in range(self.planet_object.num_orbits):
+                low_cut = (self.planet_object.times - (self.planet_object.P*(self.planet_object.num_orbits-orbits))) / U.d >= N.min(self.data[band]['t'])
+                high_cut = (self.planet_object.times - (self.planet_object.P*(self.planet_object.num_orbits-orbits))) / U.d <= N.max(self.data[band]['t'])
+                
+                plot_cut = low_cut & high_cut
+                plot_cuts.append(plot_cut)
+                plotted_times.append(self.planet_object.times[plot_cut] - (self.planet_object.P*(self.planet_object.num_orbits-orbits)))
 
             for sub_band in self.model:
                 subband_label = sub_band.replace('p', '.')
-                axis.plot(plotted_times, (self.model[sub_band]['model'].reshape(self.planet_object.time_resolution*self.planet_object.num_orbits))[plot_cut], color=color_modbg[sub_band], label=r'${0} \mu$m Model'.format(subband_label))
+                axis.plot(plotted_times[0], (self.model[sub_band]['model'].reshape(self.planet_object.time_resolution*self.planet_object.num_orbits))[plot_cuts[0]], color=color_modbg[sub_band], label=r'${0} \mu$m Model'.format(subband_label))
 
             band_label = band.replace('p', '.')
+            for i in range(self.planet_object.num_orbits-1):
+                axis.plot(plotted_times[i], (self.model[band]['model'].reshape(self.planet_object.time_resolution*self.planet_object.num_orbits))[plot_cuts[i]], color=color_modbg[band])
             axis.scatter(self.data[band]['t'], self.data[band]['flux'], color=color_datlab[band], label=r'${0} \mu$m Data'.format(band_label))
 
             axis.set_xlabel(r'$t$ (Days)')
             axis.set_ylabel(r'$\left(F/F_\star\right)_{3.6 \mu\mathrm{m}}$')
             axis.margins(0.05)
-            #axis.set_ylim([0.9995, 1.002])
+            axis.set_ylim([0.999, 1.0025])
             axis.legend(loc='upper center', fontsize=8, bbox_to_anchor=(0.5, 1.1), ncol=2, fancybox=True, shadow=True, title=r'$P_{{\mathrm{{rot}}}}\approx$ {0:.1g}, $t_{{\mathrm{{rad}}}} \sim$ {1:.2f}, $T_n=$ {2:.0f}, $A=$ {3:.2f}'.format(*self.parameters[band]))
             
             if save:
                 plt.tight_layout
-                plt.savefig('{0}_{1}_{2}.pdf'.format(datetime.date.today(), self.planet_object.name, band))
+                plt.savefig('{0}_{1}_{2}.eps'.format(datetime.date.today(), self.planet_object.name, band))
                 plt.cla()
